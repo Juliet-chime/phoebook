4714d76a2a79b157c2360aec77e0fb0325acea76
Nelson <nelsoninho11@gmail.com>
2021-12-27 19:09:51 +0100

    Added update Person feature

--- src/form.jsx
+++ src/form.jsx
@@ -1,6 +1,12 @@
 import { useState } from "react";
 
-const Form = ({ addPerson }) => {
+const Form = ({
+  addPerson,
+  isEditing,
+  currentPerson,
+  setCurrentPerson,
+  handleUpdatePerson,
+}) => {
   const [person, setPerson] = useState({
     id: 0,
     firstname: "",
@@ -27,40 +33,84 @@ const Form = ({ addPerson }) => {
     });
   };
 
+  const handleEditFormSubmit = (e) => {
+    e.preventDefault();
+    handleUpdatePerson(currentPerson.id, currentPerson);
+  };
+
+  const handleEditInputChange = (e) => {
+    setCurrentPerson({ ...currentPerson, [e.target.name]: e.target.value });
+  };
+
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="">First Name</label>
-          <input
-            type="text"
-            value={person.firstname}
-            name="firstname"
-            onChange={onChange}
-          />
-        </div>
+      {isEditing ? (
+        <form onSubmit={handleEditFormSubmit}>
+          <div>
+            <label htmlFor="">Edit Firstname</label>
+            <input
+              type="text"
+              value={currentPerson.firstname}
+              name="firstname"
+              onChange={handleEditInputChange}
+            />
+          </div>
 
-        <div>
-          <label htmlFor="">Last Name</label>
-          <input
-            type="text"
-            value={person.lastname}
-            name="lastname"
-            onChange={onChange}
-          />
-        </div>
+          <div>
+            <label htmlFor="">Edit Lastname</label>
+            <input
+              type="text"
+              value={currentPerson.lastname}
+              name="lastname"
+              onChange={handleEditInputChange}
+            />
+          </div>
 
-        <div>
-          <label htmlFor="">Phone</label>
-          <input
-            type="text"
-            value={person.phone}
-            name="phone"
-            onChange={onChange}
-          />
-        </div>
-        <button type="submit">Save</button>
-      </form>
+          <div>
+            <label htmlFor=""> Edit Phone</label>
+            <input
+              type="text"
+              value={currentPerson.phone}
+              name="phone"
+              onChange={handleEditInputChange}
+            />
+          </div>
+          <button type="submit">Save</button>
+        </form>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="">First Name</label>
+            <input
+              type="text"
+              value={person.firstname}
+              name="firstname"
+              onChange={onChange}
+            />
+          </div>
+
+          <div>
+            <label htmlFor="">Last Name</label>
+            <input
+              type="text"
+              value={person.lastname}
+              name="lastname"
+              onChange={onChange}
+            />
+          </div>
+
+          <div>
+            <label htmlFor="">Phone</label>
+            <input
+              type="text"
+              value={person.phone}
+              name="phone"
+              onChange={onChange}
+            />
+          </div>
+          <button type="submit">Save</button>
+        </form>
+      )}
     </div>
   );
 };
--- src/phonebook.jsx
+++ src/phonebook.jsx
@@ -3,6 +3,8 @@ import Form from "./form";
 
 const Phonebook = () => {
   const [persons, setPersons] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [currentPerson, setCurrentPerson] = useState({});
 
   const addPerson = (person) => {
     const { firstname, lastname, phone } = person;
@@ -11,29 +13,51 @@ const Phonebook = () => {
     }
   };
 
+  const handleEditClick = (person) => {
+    setIsEditing(true);
+    setCurrentPerson({ ...person });
+  };
+
+  const handleUpdatePerson = (id, updatedPerson) => {
+    const updatedItem = persons.map((person) =>
+      person.id === id ? updatedPerson : person
+    );
+    setIsEditing(false);
+    setPersons(updatedItem);
+  };
+
   const removePerson = (id) =>
     setPersons(persons.filter((person) => person.id !== id));
 
   return (
     <div>
-      <Form addPerson={addPerson}></Form>
+      <Form
+        addPerson={addPerson}
+        isEditing={isEditing}
+        currentPerson={currentPerson}
+        setCurrentPerson={setCurrentPerson}
+        handleUpdatePerson={handleUpdatePerson}
+      ></Form>
       <table>
         <tbody>
           <tr>
             <th>S/N</th>
-            <th>FirstName</th>
-            <th>LastName</th>
-            <th colSpan={10}>Phone</th>
-            <th colSpan>Action</th>
+            <th colSpan={1}>FirstName</th>
+            <th colSpan={1}>LastName</th>
+            <th colSpan={1}>Phone</th>
+            <th colSpan={2}>Action</th>
           </tr>
-          {persons.map((p, index) => (
-            <tr key={p.id}>
-              <td>{p.id}</td>
-              <td>{p.firstname}</td>
-              <td>{p.lastname}</td>
-              <td>{p.phone}</td>
+          {persons.map((person) => (
+            <tr key={person.id}>
+              <td>{person.id}</td>
+              <td>{person.firstname}</td>
+              <td>{person.lastname}</td>
+              <td>{person.phone}</td>
               <td>
-                <button onClick={() => removePerson(p.id)}>Delete</button>
+                <button onClick={() => handleEditClick(person)}>Edit</button>
+              </td>
+              <td>
+                <button onClick={() => removePerson(person.id)}>Delete</button>
               </td>
             </tr>
           ))}
